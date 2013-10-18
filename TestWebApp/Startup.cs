23ae3c0fcace1e14e7b5dab3642938eccd29b378
half-ogre <nemo@half-ogre.com>
2013-10-18 14:39:56 -0700

    add web app for testing

---
+++ TestWebApp/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin;
+using Owin;
+using OwinDispatcher;
+
+[assembly: OwinStartup(typeof(TestWebApp.Startup))]
+
+namespace TestWebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseDispatcher(dispatcher =>
+                dispatcher.Get("/", (environment, next) =>
+                {
+                    var response = new OwinResponse(environment)
+                    {
+                        StatusCode = 200
+                    };
+
+                    return response.WriteAsync("Home");
+                }));
+        }
+    }
+}
